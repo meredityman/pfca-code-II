a5139feec7ec449ed46dd61712bdd8ae6ec71b53
Meredith Thomas <meredityman@gmail.com>
2024-05-08 17:24:23 +0100

    WC

---
+++ week-4/main.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import os
+
+def main():
+
+    root_path = Path("./data")
+    root_path.mkdir()
+
+    if(not root_path.exists()):
+        print("Path doesn't exist!")
+        raise
+    elif(not root_path.is_dir()):
+        print("Path isn't a directory")
+        raise
+    else:
+        print("Folder exists")
+
+    
+    days = [
+        "Monday", # 0th
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday" #6th
+    ]
+
+
+    for day in days:
+        directory_path = Path(root_path, day)
+        print(directory_path)
+        directory_path.mkdir()
+
+if __name__ == "__main__":
+    main()
