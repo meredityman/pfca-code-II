0e4144c8e6c05735808a527728ec9f3510fc70ac
Meredith Thomas <meredityman@gmail.com>
2024-06-05 20:03:33 +0100

    End of week 8

--- week-8/main.py
+++ week-8/main.py
@@ -4,18 +4,34 @@ from PIL import Image
 def main(args):
     print(args.input_path)
 
+
     image = Image.open(args.input_path)
-
-    size = image.size
-
-
-    print(f"Image Size: width={size[0]}, height={size[1]}")
-
+    image.convert("RGB")
+    
+    (width, height) = image.size
     pixels = image.load()
+    print(f"Image Size: width={width}, height={height}")
+
+    sorted_image = Image.new("RGB", (width, height))
+    sorted_pixels = sorted_image.load()
 
 
-    pass
+    for y in range(height):
 
+        row = []
+        for x in range(width):
+            # Pixes 0-255
+            pix = pixels[x, y]
+            row.append(pix)
+
+        sorted_row = sorted(row, key=lambda pix: pix[0])
+
+        for x in range(width):
+            sorted_pixels[x,y] = sorted_row[x]
+
+
+    sorted_image.save("sorted_image.jpg")
+    sorted_image.show()
 
 if __name__ == "__main__":
 
